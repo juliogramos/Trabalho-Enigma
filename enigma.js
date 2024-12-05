2c3d7ff913c3f73b138e7d3fc67ede436381e9e3
juliogramos <juliogoncalvesramos@yahoo.com.br>
2024-12-05 17:18:12 -0300

    Seleção de refletor e rotores

--- enigma.js
+++ enigma.js
@@ -1,3 +1,5 @@
+const allEqual = arr => arr.every(val => val === arr[0]);
+
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // Corrigido a ordem do alfabeto
 
 const rotors = [
@@ -25,6 +27,8 @@ function setPlugboardPairs(pairs) {
         }
         return pair;
     });
+
+    return "Plugboard configurado: " + plugboardPairs.map(r => r.join(" -> "))
 }
 
 /**
@@ -42,6 +46,8 @@ function setReflectorPairs(pairs) {
         }
         return pair;
     });
+
+    return "Refletor configurado: " + reflectorPairs.map(r => r.join(" -> "))
 }
 
 /**
@@ -60,7 +66,7 @@ function setActiveRotors(order) {
         return rotors[index];
     });
 
-    console.log("Rotores ativos configurados:", activeRotors.map(r => r.join("")));
+    return "Rotores ativos configurados: " + activeRotors.map(r => r.join(""));
 }
 
 // Testando as funções
@@ -95,9 +101,10 @@ try {
 // HTML do navegador
 
 //CRIAÇÃO DINÂMICA
-//Plugboard e lampboard
+//Plugboard, refletor e lampboard
 const lampboard = document.getElementById("lampboard");
 const plugboard = document.getElementById("plugboard");
+const relector = document.getElementById("reflector");
 for (let letter of alphabet) {
     new_lamp = document.createElement("div");
     new_lamp.id = "lamp_" + letter;
@@ -108,26 +115,71 @@ for (let letter of alphabet) {
     new_plug.id = "plug_" + letter;
     new_plug.innerHTML = letter;
     plugboard.appendChild(new_plug);
+
+    new_reflector = document.createElement("div");
+    new_reflector.id = "reflect_" + letter;
+    new_reflector.innerHTML = letter;
+    reflector.appendChild(new_reflector);
 }
 
-//Seletores do plugboard
-const pair_select_1 = document.getElementById("pair_1");
-const pair_select_2 = document.getElementById("pair_2");
+// Seletores do plugboard
+const plug_pair_select_1 = document.getElementById("plug_pair_1");
+const plug_pair_select_2 = document.getElementById("plug_pair_2");
 for (let letter of alphabet) {
     new_option_1 = document.createElement("option");
     new_option_1.setAttribute("value", letter);
     new_option_1.innerHTML = letter;
-    pair_select_1.appendChild(new_option_1);
+    plug_pair_select_1.appendChild(new_option_1);
     
     new_option_2= document.createElement("option");
     new_option_2.setAttribute("value", letter);
     new_option_2.innerHTML = letter;
-    pair_select_2.appendChild(new_option_2);
+    plug_pair_select_2.appendChild(new_option_2);
+}
+
+// Seletores do refletor
+const reflect_pair_select_1 = document.getElementById("reflect_pair_1");
+const reflect_pair_select_2 = document.getElementById("reflect_pair_2");
+for (let letter of alphabet) {
+    new_option_1 = document.createElement("option");
+    new_option_1.setAttribute("value", letter);
+    new_option_1.innerHTML = letter;
+    reflect_pair_select_1.appendChild(new_option_1);
+    
+    new_option_2= document.createElement("option");
+    new_option_2.setAttribute("value", letter);
+    new_option_2.innerHTML = letter;
+    reflect_pair_select_2.appendChild(new_option_2);
+}
+
+// Rotores
+const rotor_select = document.getElementById("rotor_select");
+for (let rotor in rotors) {
+    let rotor_option = document.createElement("div");
+    rotor_option.classList.add("rotor_option");
+
+    let rotor_div = document.createElement("div");
+    rotor_div.id = "rotor_option_" + rotor;
+    rotor_div.classList.add("rotor_display");
+    for (let letter of rotors[rotor]) {
+        let letter_div = document.createElement("div");
+        letter_div.innerHTML = letter;
+        rotor_div.appendChild(letter_div);
+    }
+    rotor_option.appendChild(rotor_div);
+
+    let rotor_checkbox = document.createElement("input");
+    rotor_checkbox.setAttribute("type", "checkbox");
+    rotor_checkbox.setAttribute("value", rotor);
+    rotor_checkbox.id = "rotor_checkbox_" + rotor;
+    rotor_option.appendChild(rotor_checkbox);
+
+    rotor_select.append(rotor_option);
 }
 
 //PLUGBOARD
 // Define cores
-colors = [
+plug_colors = [
     {"color":"#e6194B", "in_use": false},
     {"color":"#f58231", "in_use": false},
     {"color":"#ffe119", "in_use": false},
@@ -144,34 +196,31 @@ colors = [
 ];
 
 //Inicializa pares
-let pairs = {};
+let plug_pairs = {};
 for (let letter of alphabet) {
-    pairs[letter] = "";
+    plug_pairs[letter] = "";
 }
 
 //Seletor de pares
-function connectPair() {
-    const pair_select_1 = document.getElementById("pair_1");
-    const pair_select_2 = document.getElementById("pair_2");
+function plugConnectPair() {
+    const pair_select_1 = document.getElementById("plug_pair_1");
+    const pair_select_2 = document.getElementById("plug_pair_2");
     const value_1 = pair_select_1.value;
     const value_2 = pair_select_2.value;
-    if (pairs[value_1] == "" && pairs[value_2] == "") {
+    if (plug_pairs[value_1] == "" && plug_pairs[value_2] == "") {
         if (value_1 == value_2) {
             alert("Não é possível selecionar a mesma letra!");
             return;
         }
-        pairs[value_1] = value_2;
-        pairs[value_2] = value_1;
-        for (let color of colors) {
+        plug_pairs[value_1] = value_2;
+        plug_pairs[value_2] = value_1;
+        for (let color of plug_colors) {
             if (color["in_use"] == false) {
-                console.log("entrou")
                 const plug_1 = document.getElementById("plug_" + value_1);
                 const plug_2 = document.getElementById("plug_" + value_2);
                 plug_1.style.borderColor = color["color"];
                 plug_2.style.borderColor = color["color"];
                 color["in_use"] = true;
-                console.log(pairs);
-                console.log(colors);
                 return;
             }
         }
@@ -181,19 +230,144 @@ function connectPair() {
 }
 
 // Limpa os pares
-function resetPairs() {
+function plugResetPairs() {
     for (let letter of alphabet) {
-        pairs[letter] = "";
+        plug_pairs[letter] = "";
         let plug = document.getElementById("plug_" + letter);
         plug.style.borderColor = "#000000";
     }
 
-    for (let color of colors) {
+    for (let color of plug_colors) {
         color["in_use"] = false;
     }
+}
 
-    console.log(pairs);
-    console.log(colors);
+//Refletor
+// Define cores
+reflect_colors = [
+    {"color":"#e6194B", "in_use": false},
+    {"color":"#f58231", "in_use": false},
+    {"color":"#ffe119", "in_use": false},
+    {"color":"#bfef45", "in_use": false},
+    {"color":"#3cb44b", "in_use": false},
+    {"color":"#42d4f4", "in_use": false},
+    {"color":"#4363d8", "in_use": false},
+    {"color":"#4363d8", "in_use": false},
+    {"color":"#911eb4", "in_use": false},
+    {"color":"#f032e6", "in_use": false},
+    {"color":"#800000", "in_use": false},
+    {"color":"#9A6324", "in_use": false},
+    {"color":"#808000", "in_use": false}
+];
+
+//Inicializa pares
+let reflect_pairs = {};
+for (let letter of alphabet) {
+    reflect_pairs[letter] = "";
+}
+
+//Seletor de pares
+function reflectConnectPair() {
+    const pair_select_1 = document.getElementById("reflect_pair_1");
+    const pair_select_2 = document.getElementById("reflect_pair_2");
+    const value_1 = pair_select_1.value;
+    const value_2 = pair_select_2.value;
+    if (reflect_pairs[value_1] == "" && reflect_pairs[value_2] == "") {
+        if (value_1 == value_2) {
+            alert("Não é possível selecionar a mesma letra!");
+            return;
+        }
+        reflect_pairs[value_1] = value_2;
+        reflect_pairs[value_2] = value_1;
+        for (let color of reflect_colors) {
+            if (color["in_use"] == false) {
+                const reflect_1 = document.getElementById("reflect_" + value_1);
+                const reflect_2 = document.getElementById("reflect_" + value_2);
+                reflect_1.style.borderColor = color["color"];
+                reflect_2.style.borderColor = color["color"];
+                color["in_use"] = true;
+                return;
+            }
+        }
+    } else {
+        alert("Par já selecionado! Limpe os pares para redefinir este par.")
+    }
+}
+
+// Limpa os pares
+function reflectResetPairs() {
+    for (let letter of alphabet) {
+        reflect_pairs[letter] = "";
+        let reflect = document.getElementById("reflect_" + letter);
+        reflect.style.borderColor = "#000000";
+    }
+
+    for (let color of reflect_colors) {
+        color["in_use"] = false;
+    }
+}
+
+// SALVAR CONFIGURAÇÕES
+function saveConfigs() {
+    // Salva rotores
+    const trueRotors = [
+        document.getElementById("rotor_checkbox_0").checked,
+        document.getElementById("rotor_checkbox_1").checked,
+        document.getElementById("rotor_checkbox_2").checked
+    ]
+
+    console.log(trueRotors);
+    
+    if (allEqual(trueRotors) && trueRotors[0] == false) {
+        alert("Selecione pelo menos um rotor!");
+        return;
+    }
+
+    let selectedRotors = [];
+
+    for (let i = 0; i < trueRotors.length; i++) {
+        if (trueRotors[i] == true) {
+            selectedRotors.push(i);
+        }
+    }
+
+    //Salva plugboard e refletores
+    let plugTuples = [];
+    let reflectTuples = [];
+
+    for (let letter of alphabet) {
+        if (plug_pairs[letter] != "") {
+            plugTuples.push([letter, plug_pairs[letter]]);
+        }
+
+         if (reflect_pairs[letter] != "") {
+            reflectTuples.push([letter, reflect_pairs[letter]]);
+        }
+    }
+
+    let rotormsg;
+    let plugmsg;
+    let reflectmsg;
+
+    try {
+        rotormsg = setActiveRotors(selectedRotors);
+        plugmsg = setPlugboardPairs(plugTuples);
+        reflectmsg = setReflectorPairs(reflectTuples);
+    } catch(error) {
+        alert(error.message);
+        return;
+    }
+
+    const whichRotors = document.getElementById("whichRotors");
+    const whichPlugs = document.getElementById("whichPlugs");
+    const whichReflects = document.getElementById("whichReflects");
+
+    whichRotors.innerHTML = rotormsg;
+    whichPlugs.innerHTML = plugmsg;
+    whichReflects.innerHTML = reflectmsg;
+
+    const canType = document.getElementById("canType");
+    canType.style.display = "flex";
 }
 
 // LAMPBOARD
